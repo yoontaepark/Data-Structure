f4d5b6bdc79922c1562a4e556aad06f212902433
yoontaepark <65579629+yoontaepark@users.noreply.github.com>
2020-09-22 22:30:52 +0900

    Create Array1.c

---
+++ Chap2/Array1.c
@@ -0,0 +1,10 @@
+//Creating Array 
+void create(int n) {      //n=5
+  int a[n];
+  int i;
+  for(i=0, i<n, i++){
+    a[i] = 0;
+  }
+}
+
+// if I put n=5, then a[0], a[1], a[2], a[3], a[4] = 0 is created. 
