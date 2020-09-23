a98186ec425408f41a1935e5f640584efbc2ed84
yoontaepark <65579629+yoontaepark@users.noreply.github.com>
2020-09-23 22:06:52 +0900

    Create Array2.c

---
+++ Chap2/Array2.c
@@ -0,0 +1,9 @@
+// Search array(retrieve) 
+#define array_size 5
+int retrieve(int *a, int i){    //i=2
+  if(i>=0 && i<array size) return a[i];
+  else {
+  printf("Error\n");
+  return(-1);
+  }
+}
