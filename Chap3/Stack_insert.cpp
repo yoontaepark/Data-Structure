77de916c2a2bb6b962003deb289f1aaca0e395a9
yoontaepark <65579629+yoontaepark@users.noreply.github.com>
2020-09-28 23:59:50 +0900

    Update Stack_insert.cpp

--- Chap3/Stack_insert.cpp
+++ Chap3/Stack_insert.cpp
@@ -1,7 +1,18 @@
-void push(int* stack, int* top, element item) {
-	if (*top >= STACK_SIZE - 1) {
-		printf("Stack is Full!! \n");
-		return;
+#include <iostream>
+
+//스택 생성
+#define stack_size 10
+typedef int element;
+element stack[stack_size];
+int top = -1;
+int item = 400;
+
+int main() {
+	void push(int *stack, int *top, element item); {  //스택 삽입, ITEM은 400으로 가정
+		if (top >= stack_size - 1) {				  //스택이 FULL 인경우 에러메시지 호출
+			printf("Stack is Full!! \n");
+			return 0;
+		}
+		else stack[++ top] = item;
 	}
-	else stack[++top] = item;
 }
