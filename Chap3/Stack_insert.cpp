58cb7f7be25e17a068fdfb79cb0bfb024ac5e3b0
yoontaepark <65579629+yoontaepark@users.noreply.github.com>
2020-09-28 23:45:18 +0900

    Update Stack_insert.cpp

--- Chap3/Stack_insert.cpp
+++ Chap3/Stack_insert.cpp
@@ -1,7 +1,7 @@
 void push(int* stack, int* top, element item) {
-		if (*top >= STACK_SIZE - 1) {
-			printf("Stack is Full!! \n");
-			return;
-		}
-		else stack[++top] = item;
+	if (*top >= STACK_SIZE - 1) {
+		printf("Stack is Full!! \n");
+		return;
 	}
+	else stack[++top] = item;
+}
