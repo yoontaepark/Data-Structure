d0d95e56293b1fdc811a1faf5aa60696047f2dbe
yoontaepark <65579629+yoontaepark@users.noreply.github.com>
2020-09-29 23:11:04 +0900

    Update Stack_del.cpp

--- Chap3/Stack_del.cpp
+++ Chap3/Stack_del.cpp
@@ -1,6 +1,8 @@
 //Delete Stack
 element pop(int *top){
-  if(*top == -1)
-    return stackEmpty();
-  return stack[(*top)--];
+  if(*top == -1){
+    printf("Stack is Empty!!\n");
+    return 0;
+  }
+  else return stack[top--];
 }
