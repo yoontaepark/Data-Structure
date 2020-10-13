01ab143ce47a1a12b5e1f79a92015f350a7b2f1f
yoontaepark <65579629+yoontaepark@users.noreply.github.com>
2020-10-13 22:41:42 +0900

    Create Search node.cpp

---
+++ Chap5/Search node.cpp
@@ -0,0 +1,10 @@
+void searchNode(linkedList_h* H, int itdata){
+//연결 리스트에서 특정 노드를 검색하여 반환하고자 하는 연산, itdata=200
+  listNode* tempNode;
+  tempNode = H → head'
+  
+  while(tempNode → data != itdat){
+    tempNode = tempNode → link;
+  }
+  printf("Search successful!! \n:);
+}
