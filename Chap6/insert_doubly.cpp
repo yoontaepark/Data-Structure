43a481cac19075a4b1812d5eb22bee58bd90c4ec
yoontaepark <65579629+yoontaepark@users.noreply.github.com>
2020-10-23 23:58:07 +0900

    Update insert_doubly.cpp

--- Chap6/insert_doubly.cpp
+++ Chap6/insert_doubly.cpp
@@ -1,1 +1,14 @@
-void addDNode(linkedLish_h*
+void addDNode(linkedLish_h* H, listNode* prevNode, int x) {
+//이중 연결 리스트 노드 삽입 연산, x값은 200이라고 가정함
+  listNode* NewNode;
+  NewNode = (listNode*)malloc(sizeof(listNode));
+  NewNode → Llink = NULL;
+  NewNode → data = x;
+  NewNode → Rlink = NULL;
+
+  NewNode → Rlink = prevNode → Rlink;
+  NewNode → Rlink = NewNode;
+  NewNode → Llink = prevNode;
+  NewNode → Rlink → Llink = NewNode;
+}
+  
