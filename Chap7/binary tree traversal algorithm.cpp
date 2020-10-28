a4ec2e2f52cd5a7af3dfe5e5af6b1618026b21a3
yoontaepark <65579629+yoontaepark@users.noreply.github.com>
2020-10-28 21:51:37 +0900

    Create binary tree traversal algorithm.cpp

---
+++ Chap7/binary tree traversal algorithm.cpp
@@ -0,0 +1,25 @@
+struct node *nodeptr;
+
+void preorder(struct node *tree_ptr){
+  if(tree_ptr){
+    printf("%d", tree_ptr→info);
+    preorder(tree_ptr→left);
+    preorder(tree_ptr→right);
+  }
+}
+
+void inorder(struct node *tree_ptr){
+  if(tree_ptr){
+    inorder(tree_ptr→left);
+    printf("%d", tree_ptr→info);
+    inorder(tree_ptr→right);
+  }
+}
+
+void postorder(struct node *tree_ptr){
+  if(tree_ptr){
+    postorder(tree_ptr→left);
+    postorder(tree_ptr→right);
+    printf("%d", tree_ptr→info);
+  }
+}
