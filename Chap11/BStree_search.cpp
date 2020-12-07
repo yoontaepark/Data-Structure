b29c85265775f6c9cfdf78c28ef6630d710b5903
yoontaepark <65579629+yoontaepark@users.noreply.github.com>
2020-12-07 23:05:20 +0900

    Create BStree_search.cpp

---
+++ Chap11/BStree_search.cpp
@@ -0,0 +1,10 @@
+struct KNode *Search (char K[], struct KNode *r){
+  if(r == NULL)
+    return(NULL);
+  else
+    if(strcmp(k, r→key) == 0)
+      return(r);
+    else if(strcmp(k, r→key) < 0)
+      return(Search(k, r→left));
+    else return(Search(k, r→right));
+}
